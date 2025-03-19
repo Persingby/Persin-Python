d2ce601870b0232228add6962f24d7bd42d94d5e
Persingby <gabriely.persin.silva@escola.pr.gov.br>
2025-03-19 11:14:32 -0300

    Add files via upload

--- persin/aula29.py
+++ persin/aula29.py
@@ -10,5 +10,33 @@ def cadastrar_pessoa(cadastros):
     idade = input("Idade:")
     turma = input("Turma:")
     curso = input("Curso:")
-    cadastros.append({"Nome" : nome, "idade" : idade})
+    cadastros.append({"Nome" : Nome, "idade" : idade, "Turma" : turma})
     print("Cadastro realizado com sucesso!")
+
+def ver_cadastro(cadastros):
+    if not cadastros:
+        print("/n Nenhum cadastro no sistema")
+    else:
+        print("/n------Lista de cadastros------")
+
+        for i, pessoa in enumerate (cadastros, 1):
+            print(f"{i}. nome: {pessoa['nome']}, idade: {pessoa['idade']}, Turma {pessoa['Turma']}, Curso{pessoa['Curso']}")
+
+def main():
+    cadastro = [ ]
+    while True:
+        exibir_menu ()
+        opção = input("Escolha uma opção: ")
+        if opção == "1":
+            cadastrar_pessoa (cadastro)
+        elif opção == "2":
+            ver_cadastro (cadastro)
+        elif opção == "3":
+            print ("Obrigado por utilizar nosso sistema!")
+            break
+        else:
+            print("Opção incorreta, tente de novo.")
+
+
+if__name__ == "__main_":
+    main()
